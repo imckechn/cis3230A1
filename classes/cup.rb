a7f63e88ea73bd5cf3710557aab2a4aca14f1f4e
Ian McKechnie <imckechn@uoguelph.ca>
2022-10-01 12:22:16 -0400

    Another test commit

--- classes/cup.rb
+++ classes/cup.rb
@@ -8,6 +8,7 @@ class Cup < RandomizerContainer
         @item = Hash["item" => "cup"] #Item specifies the name of the object
         self._init()
     end
+    
 
     #enters each randomizer from a Hand (synonym of move_all() )
     def load(hand:Hand)
@@ -15,12 +16,14 @@ class Cup < RandomizerContainer
         return self
     end
 
+
     #returns a Hand object to be returned to the bag, and leaves the cup empty
     def empty()
         hand = hand.new()
         empty()
         return hand
     end
+
 
     #each randomizer in the cup is rolled or flipped
     #the results of the thrown randomizers are stored in a newly created Results object, which is returned
